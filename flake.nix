6402bb839667989eab2c6ee466feab581f4efcb3
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-10-14 19:37:55 +0200

    Use Nix to build and publish

--- flake.nix
+++ flake.nix
@@ -9,11 +9,24 @@
     nixpkgs,
     flake-utils,
   }:
-  flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [ zola ];
-    };
-  });
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ zola ];
+      };
+
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "linuxday-messina-website";
+        version = "2025-10-25";
+        src = ./.;
+        nativeBuildInputs = [ pkgs.zola ];
+        buildPhase = ''
+          mkdir -p $out
+          zola build --force --output-dir $out
+        '';
+      };
+    }
+  );
 }
